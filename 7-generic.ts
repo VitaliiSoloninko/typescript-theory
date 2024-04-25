c1eacc3ad5ecea91e994ea8d23df7d729c259605
22051988Di <vitalii.soloninko@gmail.com>
2024-04-25 17:38:48 +0200

    Generic	Part 1	work with different types of data

--- 7-generic.ts
+++ 7-generic.ts
@@ -1,9 +1,16 @@
 // Generic - загальний
 
 const arrayOfNumbers: Array<number> = [1, 1, 2, 3, 5]
+const arrayOfStrings: Array<string> = ['Hello', 'Vitalii']
+// Array - object
+// <number> - з чого складається
 
-// функція яка працює з різними типами даних
+// масив може бути із чисел, рядків і змішані - тапос
+// дані  буде функція, яка працює з різними типами даних
 
-function reverse(array) {
+function reverse<T>(array: T[]): T[] {
 	return array.reverse()
 }
+
+reverse(arrayOfNumbers)
+reverse(arrayOfStrings)
